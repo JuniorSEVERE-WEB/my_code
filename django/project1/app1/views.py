a2fa3b4579113c60d1f00ff90661c8d17ffad711
Junior SEVERE <severejunior2017@gmail.com>
2025-04-18 06:44:15 -0700

    third-urls-views

--- django/project1/app1/views.py
+++ django/project1/app1/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello world!")
 
 def months(request, month):
-    return HttpResponse(f"we are in {month}")
+    if month.lower() == "january":
+        message_month = f"we are in {month}, we eat rice"
+    elif month.lower() == "february":
+        message_month = f"we are in {month}, we eat beans"
+    elif month.lower() == "march":
+        message_month = f"we are in {month}, we eat pineaple"
+    else:
+        return HttpResponseNotFound("your input is not in the first month")
+    return HttpResponse(message_month)            
+        
