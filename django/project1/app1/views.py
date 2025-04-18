995ea7638af83941a396da6c8227f737333cdcdc
Junior SEVERE <severejunior2017@gmail.com>
2025-04-18 05:36:51 -0700

    first-urls-views

--- django/project1/app1/views.py
+++ django/project1/app1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("we are in january")
+
+def february(request):
+    return HttpResponse("We are in february")
+
+def march(request):
+    return HttpResponse("We are in march")
