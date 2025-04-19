124aac3c5848405261428783cd79aa6c6ad58828
Junior SEVERE <severejunior2017@gmail.com>
2025-04-18 19:57:20 -0700

    adjust month index to zero-based in redirect

--- django/project1/app1/views.py
+++ django/project1/app1/views.py
@@ -19,7 +19,7 @@ months_dict = {
 
 def months_by_number(request, month):
     the_months = list(months_dict.keys())
-    redirect_month = the_months[month]
+    redirect_month = the_months[month - 1]
     return HttpResponseRedirect("/app1/" + redirect_month)
 
 
@@ -29,7 +29,7 @@ def months_by_string(request, month):
         message_month = months_dict[month.lower()]
         return HttpResponse(message_month)
           
-    except KeyError:
+    except:
         return HttpResponseNotFound("This month is not supported, okok!")    
    
              
