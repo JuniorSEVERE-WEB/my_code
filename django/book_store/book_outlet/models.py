af6a3dedd2ff36cc499312ada2de79b7562c94b0
Junior SEVERE <severejunior2017@gmail.com>
2025-05-21 18:53:45 -0700

    rendering queried data in the template

--- django/book_store/book_outlet/models.py
+++ django/book_store/book_outlet/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null = True, max_length=100)
+    author = models.CharField(max_length=100)
     is_bestselling = models.BooleanField(default=False)
     
     def __str__(self):
