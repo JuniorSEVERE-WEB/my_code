1b62bbb465a7cf1ef1203747acf022eba0b5749e
Junior SEVERE <severejunior2017@gmail.com>
2025-05-27 18:24:49 -0700

    many-to-many-1

--- django/airline/flights/views.py
+++ django/airline/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
+from django.http import HttpResponseRedirect 
+from django.urls import reverse 
 
 
 # Create your views here.
@@ -14,5 +16,12 @@ def flight(request, flight_id):
         "flight": flight,
         "passengers": flight.passengers.all()
     })
+    
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)    
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id, )))
         
     
